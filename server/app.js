8e48440a5d7128d924ea359f56eef84fb9825c1a
Shivom paul <151982982+Shivom76@users.noreply.github.com>
2025-12-22 23:19:48 +0530

    Update app.js cors

--- server/app.js
+++ server/app.js
@@ -8,7 +8,11 @@ const {todoRoutes}=require('./routes/todoRoutes.js')
 const mongoose=require('mongoose')
 
 app.use(express.json())
-app.use(cors())
+app.use(cors({
+    origin: 'https://kanban-board-gray-eta.vercel.app', // Your frontend URL
+    methods: ['GET', 'POST', 'PATCH', 'DELETE'],
+    credentials: true
+}))
 
 // mongoose.connect('mongodb://127.0.0.1:27017/taskboard')
 mongoose.connect(process.env.DB_URL)
