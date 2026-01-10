b575fcc01cd5e3fb97d4fcc441ccad682d14c0ca
Shivom76 <shivompaul76@gmail.com>
2026-01-10 14:29:51 +0530

    error management

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -38,12 +38,12 @@ const Login=({setIsLoggedIn})=>{
     return(
         <div className="min-h-screen flex flex-col justify-center items-center">
             <form onSubmit={loginFunction} className="max-w-md bg-white flex justify-center flex-col gap-6 border-2 border-black items-center p-6 w-11/12 md:w-4/12 rounded-lg">
-                <div id="username">
+                <div id="usernameContainer">
                     <label htmlFor="username">Username:</label>
                     <input name="username" id="username" type="text" onChange={textInput} autoComplete="username" placeholder="Enter username" className="w-5/6 border p-4 rounded-lg rounded-tr-none focus:outline-none focus:ring-2 focus:ring-blue-400"></input>
                 </div>
 
-                <div id="username">
+                <div id="passwordContainer">
                     <label htmlFor="password">Password:</label>
                     <input name="password" id="password" type="password" onChange={textInput} autoComplete="current-password" placeholder="Enter password" className="w-5/6 border p-4 rounded-lg rounded-tr-none focus:outline-none focus:ring-2 focus:ring-blue-400"></input>
                 </div>
